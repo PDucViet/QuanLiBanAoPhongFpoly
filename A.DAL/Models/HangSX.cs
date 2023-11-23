73569465ebeed4f113ae0aae9ee429e8fc503171
DUC VIET <DUC VIET@DESKTOP-A8J0KMC>
2023-11-23 15:33:16 +0700

    Update Model & DBContext

---
+++ A.DAL/Models/HangSX.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A.DAL.Models
+{
+    public class HangSX
+    {
+        [Key] public Guid MaHSX { get; set; }
+        public string TenHSX { get; set; }
+        public bool TrangThaiHSX { get; set; }
+        public List<AoPhongCT> AoPhongCT { get; set; }
+    }
+}
---
+++ A.DAL/Models/HoaDonChiTiet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A.DAL.Models
+{
+    public class HoaDonChiTiet
+    {
+        [Key]public Guid MaHdct { get; set; }
+        public bool TrangThaiHdct { get; set; }
+        public HoaDon HoaDon { get; set; }
+        public AoPhongCT AoPhongCT { get; set; }
+    }
+}
