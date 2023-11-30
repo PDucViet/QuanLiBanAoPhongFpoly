c0aeb1f97dbf2c859098ecf06c0b86c5084dc702
DUC VIET <DUC VIET@DESKTOP-A8J0KMC>
2023-11-30 22:21:09 +0700

    Update Form QuenMK

---
+++ C.PL/FUNC/MailSupport.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+namespace C.PL.FUNC
+{
+    public class MailSupport
+    {
+        public string EMAIL { get; set; }
+        public string PASSWORD { get; set; }
+        public MailSupport(string mail, string _pass)
+        {
+            EMAIL = mail;
+            PASSWORD = _pass;
+        }
+
+        public void send(string mailto, string _subject, string content)
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");// 
+
+            mail.From = new MailAddress(EMAIL);
+            mail.To.Add(mailto);
+            mail.Subject = _subject;
+            mail.IsBodyHtml = true;
+            mail.Body = content;
+
+            SmtpServer.Port = 465;
+            SmtpServer.Credentials = new System.Net.NetworkCredential(EMAIL, PASSWORD);
+            SmtpServer.EnableSsl = true;
+            
+            SmtpServer.Send(mail);
+
+        }
+    }
+}
